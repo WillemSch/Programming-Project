2b6be15f8e1dfa8096d6789f803ae114309b4197
Jesper Simon <Jesper Simon@LAPTOP-FG8IV0D6>
2017-01-27 14:15:53 +0100

    fuu

--- Programming project/src/game/ConnectFour.java
+++ Programming project/src/game/ConnectFour.java
@@ -5,7 +5,7 @@ public class ConnectFour {
 		Player p1 = new HumanPlayer(args[0], Mark.XX);
 		Player p2 = new HumanPlayer(args[1], Mark.OO);
 		Player[] players = {p1,p2};
-		Board board = new Board(1,1,1,5,players);
+		Board board = new Board(4,4,1,3,players);
 		Game game = new Game(board);
 		game.start();
 	}
