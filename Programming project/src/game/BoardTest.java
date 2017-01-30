98022db50dbc23f518849308a7da1f693ea34967
willem <schooltink.willem@gmailcom>
2017-01-30 20:15:53 +0100

    Buy your skooma here

--- Programming project/src/game/BoardTest.java
+++ Programming project/src/game/BoardTest.java
@@ -4,13 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Created by willem on 30-1-17.
+ * @author willem
+ * @version 1.0.0
+ * Tests the functionality of the <code>Board</code> class, with 100% coverage.
  */
 public class BoardTest {
     private Board board;
     private Color color1;
     private Color color2;
 
+    /**
+     * Setup the board and global variables.
+     */
     @BeforeEach
     public void setUp(){
         board = new Board(4, 4, 4);
@@ -18,6 +23,9 @@ public class BoardTest {
         color2 = Color.RED;
     }
 
+    /**
+     * Test both setField() methods in <code>Board</code>.
+     */
     @Test
     public void testSetField(){
         int[] coordinates = {0,0,0};
@@ -29,6 +37,9 @@ public class BoardTest {
         assert (board.getField(coordinates2).equals(color1));
     }
 
+    /**
+     * Test the getHeightOfField() method in <code>Board</code>.
+     */
     @Test
     public void testGetHeightOfField(){
         int x = 0;
@@ -40,6 +51,9 @@ public class BoardTest {
         assert (board.getHeightOfField(x,y) == 1);
     }
 
+    /**
+     * Test the isField() method in <code>Board</code>.
+     */
     @Test
     public void testIsField(){
         int[] isRealField = {0,0,0};
@@ -51,6 +65,9 @@ public class BoardTest {
         assert (!board.isField(isNotAField2));
     }
 
+    /**
+     * Test the isEmptyField() method in <code>Board</code>.
+     */
     @Test
     public void testIsEmptyField(){
         int[] coordinates = {0,0,0};
@@ -61,6 +78,9 @@ public class BoardTest {
         assert (!board.isEmptyField(coordinates));
     }
 
+    /**
+     * Test the reset() method in <code>Board</code>.
+     */
     @Test
     public void testBoardReset(){
         int[] coordinates = {0,0,0};
@@ -70,6 +90,9 @@ public class BoardTest {
         assert (board.isEmptyField(coordinates));
     }
 
+    /**
+     * Test the hasWinner() and isWinner(Color) method in <code>Board</code>.
+     */
     @Test
     public void testWinner(){
         int x = 0;
@@ -78,11 +101,12 @@ public class BoardTest {
         assert (!board.hasWinner());
 
         board.setField(0,0,color1);
-        board.setField(0,0,color1);
-        board.setField(0,0,color1);
-        board.setField(0,0,color1);
+        board.setField(1,1,color1);
+        board.setField(2,2,color1);
+        board.setField(3,3,color1);
 
         assert (board.hasWinner());
         assert (board.isWinner(color1));
+        assert (!board.isWinner(color2));
     }
 }
